cb39d297a99aa79b77eafd7d65dcca308da1f28c
Eliana Laudadio <elaudadi@rams.colostate.edu>
2023-03-19 11:30:16 -0500

    Update FirstPerson.cs

--- HCI project/Assets/FirstPerson.cs
+++ HCI project/Assets/FirstPerson.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPerson : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class FirstPerson : MonoBehaviour {
+   public float sensX;
+   public float sensY;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public Transform orientation;
+
+   float xRotation;
+   float yRotation;
+
+   private void Start(){
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+   }
+
+   private void Update(){
+    //get mouse input
+    float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+    float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+    yRotation += mouseX;
+    
+    xRotation -= mouseY;
+    xRotation = Mathf.Clamp(xRotation, -90f, 90);
+    
+    //rotate cam and orientation
+    transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);      
+    orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+   }
 }
+
